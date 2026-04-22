2cf7721db546e8a0999736f278bb3ce7b04d84e8
OmarAyman <omaraymanzaid1122@gmail.com>
2026-04-22 14:47:17 +0200

    Remove build_dataset function from utils.py, and put it in main

--- modules/utils.py
+++ modules/utils.py
@@ -28,52 +28,6 @@ def load_image_paths(data_path, category="bottle", split="train", max_images=Non
 
     return image_paths
 
-def build_dataset(data_path, category, max_samples=50):
-
-
-    import os, cv2
-    from tqdm import tqdm
-
-    X, y = [], []
-
-    test_path = os.path.join(data_path, category, "test")
-
-    for folder in os.listdir(test_path):
-        folder_path = os.path.join(test_path, folder)
-
-        if not os.path.isdir(folder_path):
-            continue
-
-        label = 0 if folder == "good" else 1
-
-        images = os.listdir(folder_path)[:max_samples]
-
-        for img_name in tqdm(images, desc=f"{folder}", leave=False):
-            img_path = os.path.join(folder_path, img_name)
-            img = cv2.imread(img_path)
-
-            if img is None:
-                continue
-
-            try:
-                # Preprocess
-                pre = preprocess_image(img)
-                processed = pre["median"]
-
-                # Segment
-                mask = segment_image(processed, defect_type=folder)
-
-                # Features
-                feats = extract_features(processed, mask, pre["metrics"])
-
-                X.append(feats)
-                y.append(label)
-
-            except Exception as e:
-                print(f"Error: {e}")
-
-    return np.array(X), np.array(y)
-
 # -----------------------------
 # Detect environment
 # -----------------------------
